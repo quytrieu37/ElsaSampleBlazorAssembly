66e00d4039c6f9e77d70da6524c9607db364f3b4
TrieuNQ <quytrieu1379@gmail.com>
2024-07-24 15:33:12 +0700

    first commit

---
+++ SampleBlazorAssembly.UIHint/Components/TreeUIHint.razor.cs
@@ -0,0 +1,11 @@
+﻿using Elsa.Studio.Components;
+using Elsa.Studio.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace SampleBlazorAssembly.UIHint.Components;
+
+public partial class TreeUIHint : StudioComponentBase
+{
+    [Parameter]
+    public DisplayInputEditorContext EditorContext { get; set; } = default!;
+}
---
+++ SampleBlazorAssembly.UIHint/Handlers/TreeUIHintHandler.cs
@@ -0,0 +1,23 @@
+﻿using Elsa.Studio;
+using Elsa.Studio.Contracts;
+using Elsa.Studio.Models;
+using Microsoft.AspNetCore.Components;
+using SampleBlazorAssembly.UIHint.Components;
+
+namespace SampleBlazorAssembly.UIHint.Handlers;
+
+public class TreeUIHintHandler : IUIHintHandler
+{
+    public bool GetSupportsUIHint(string uiHint) => uiHint == "tree";
+    public string UISyntax => WellKnownSyntaxNames.Literal;
+
+    public RenderFragment DisplayInputEditor(DisplayInputEditorContext context)
+    {
+        return builder =>
+        {
+            builder.OpenComponent(0, typeof(TreeUIHint));
+            builder.AddAttribute(1, nameof(TreeUIHint.EditorContext), context);
+            builder.CloseComponent();
+        };
+    }
+}
